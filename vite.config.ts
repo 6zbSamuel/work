377349aa3facc9e9867862f8417b8224bb7c6e3a
6zbSamuel <134755875+6zbSamuel@users.noreply.github.com>
2024-11-19 00:14:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+      host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
